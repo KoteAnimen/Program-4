8e534f40075b4451f9ce84a14c9fee3c502846c0
KoteAnimen <sverh.il@yandex.ru>
2021-03-10 12:33:31 +0300

    Снова изменен тест

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -10,12 +10,12 @@ namespace UnitTests
         [TestMethod]
         public void TestMethod1()
         {
-            task.Task task1 = new task.Task(1, -1, 1);
+            task.Task task1 = new task.Task(1, 1, 1);
             int positive;
             int negative;
             task1.AnswerTask(out positive, out negative);
-            Assert.AreEqual(2, positive);
-            Assert.AreEqual(1, negative);
+            Assert.AreEqual(3, positive);
+            Assert.AreEqual(0, negative);
         }
         [TestMethod]
         public void TestMethod2()
