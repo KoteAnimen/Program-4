dbc067a5860dcda8fdd486e3fa3804dc2abfea8b
KoteAnimen <sverh.il@yandex.ru>
2021-03-10 12:23:14 +0300

    Тестовые методы изменены

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -10,33 +10,24 @@ namespace UnitTests
         [TestMethod]
         public void TestMethod1()
         {
-            task.Task task1 = new task.Task(1, 1, 1);
+            task.Task task1 = new task.Task(1, -1, 1);
             int positive;
             int negative;
             task1.AnswerTask(out positive, out negative);
-            Assert.AreEqual(3, positive);
-            Assert.AreEqual(0, negative);
+            Assert.AreEqual(2, positive);
+            Assert.AreEqual(1, negative);
         }
         [TestMethod]
         public void TestMethod2()
         {
-            task.Task task1 = new task.Task(-1, -1, -1);
+            task.Task task1 = new task.Task(-1, -1, 0);
             int positive;
             int negative;
             task1.AnswerTask(out positive, out negative);
             Assert.AreEqual(0, positive);
-            Assert.AreEqual(3, negative);
+            Assert.AreEqual(2, negative);
         }
 
-        [TestMethod]
-        public void TestMethod3()
-        {
-            task.Task task1 = new task.Task(0, 0, 0);
-            int positive;
-            int negative;
-            task1.AnswerTask(out positive, out negative);
-            Assert.AreEqual(0, positive);
-            Assert.AreEqual(0, negative);
-        }
+        
     }
 }
